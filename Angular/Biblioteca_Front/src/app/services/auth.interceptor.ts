01772115d7aad1cd86cd095ae0934ab02b4e9d07
javier muñoz gallego <jmgprogramador@gmail.com>
2025-06-05 00:41:21 +0200

    css al portatil

--- Angular/Biblioteca_Front/src/app/services/auth.interceptor.ts
+++ Angular/Biblioteca_Front/src/app/services/auth.interceptor.ts
@@ -1,5 +1,5 @@
 import { HttpInterceptorFn } from '@angular/common/http';
-
+//intercepta todas las peticiones y le añade un token de autorización si existe en el localStorage
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('jwt');
   if (token) {
